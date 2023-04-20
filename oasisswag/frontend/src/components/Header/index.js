b9ac5703565ecd48666cd21a5a57a3538a6d1493
lubej <9722540+lubej@users.noreply.github.com>
2023-04-20 09:43:29 +0200

    Update header

--- oasisswag/frontend/src/components/Header/index.js
+++ oasisswag/frontend/src/components/Header/index.js
@@ -17,35 +17,38 @@ export const Header = () => {
     <header className={classes.header}>
       <img className={classes.logo} src={logo} alt="Logo"/>
       <div className={classes.headerContent}>
-        <h1 className={classes.headerTitle}>OASIS SWAG WHEEL</h1>
-        <h2 className={classes.headerSubTitle}>
-          Spin the wheel and our Sapphire native RNG will determine which swag item you won.
-        </h2>
-        {!isMetamaskInstalled &&
-          <Button onClick={navigateToMetamask}>
-            <>
-              <img src={wallet} alt="Wallet"/>
-              <span>Install metamask</span>
-            </>
-          </Button>
-        }
-        {isMetamaskInstalled && !selectedAddress &&
-          <>
-            <Button onClick={addSapphireNetworkToMetamask}>
-              Add network
-            </Button>
-            <br/>
-            <br/>
-            <Button onClick={connectWallet}>
+        <div className={classes.headerContentLeft}>
+          <h1 className={classes.headerTitle}>OASIS<br /> SWAG WHEEL</h1>
+          <h2 className={classes.headerSubTitle}>
+            Spin the wheel and our Sapphire native RNG will determine which swag item you won.
+          </h2>
+          {!isMetamaskInstalled &&
+            <Button onClick={navigateToMetamask}>
               <>
                 <img src={wallet} alt="Wallet"/>
-                <span>Connect Wallet</span>
+                <span>Install metamask</span>
               </>
             </Button>
-            <br/>
-            {networkError && <ErrorMessage>{networkError}</ErrorMessage>}
-          </>
-        }
+          }
+          {isMetamaskInstalled && !selectedAddress &&
+            <>
+              <Button onClick={addSapphireNetworkToMetamask}>
+                Add network
+              </Button>
+              <br/>
+              <br/>
+              <Button onClick={connectWallet}>
+                <>
+                  <img src={wallet} alt="Wallet"/>
+                  <span>Connect Wallet</span>
+                </>
+              </Button>
+              <br/>
+              {networkError && <ErrorMessage>{networkError}</ErrorMessage>}
+            </>
+          }
+        </div>
+        <div className={classes.headerContentRight}></div>
       </div>
     </header>
   )
