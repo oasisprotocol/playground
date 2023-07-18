c411bdf1669b96ba080f2bb86d3af89346b83be8
Matevž Jekovec <matevz@oasisprotocol.org>
2023-07-18 17:26:31 +0200

    demo-voting: Fix pinata key

--- demo-voting/assets/404View.c7dd3715.js
+++
@@ -1,2 +0,0 @@
-import{_ as e,o,c as s,a as t}from"./index.37d64b81.js";const c={},n={class:"404"},_=t("h1",null,"Page not found",-1),a=[_];function r(d,i){return o(),s("div",n,a)}const l=e(c,[["render",r]]);export{l as default};
-//# sourceMappingURL=404View.c7dd3715.js.map
