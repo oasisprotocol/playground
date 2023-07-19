3c350e031fad8c4cc91a3d259e569f5a96b99964
Matevž Jekovec <matevz@oasisprotocol.org>
2023-07-19 11:31:50 +0200

    demo-voting: Multi-account test

--- demo-voting/assets/404View.07b83725.js
+++
@@ -1,2 +0,0 @@
-import{_ as e,o,c as s,a as t}from"./index.d49c0a1c.js";const c={},n={class:"404"},_=t("h1",null,"Page not found",-1),a=[_];function r(d,i){return o(),s("div",n,a)}const l=e(c,[["render",r]]);export{l as default};
-//# sourceMappingURL=404View.07b83725.js.map
---
+++ demo-voting/assets/404View.4bf3980b.js
@@ -0,0 +1,2 @@
+import{_ as e,o,c as s,a as t}from"./index.dae7de47.js";const c={},n={class:"404"},_=t("h1",null,"Page not found",-1),a=[_];function r(d,i){return o(),s("div",n,a)}const l=e(c,[["render",r]]);export{l as default};
+//# sourceMappingURL=404View.4bf3980b.js.map
