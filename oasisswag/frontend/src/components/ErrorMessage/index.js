636b9c3dd0c26b14887d5d1bf1d965a92d5b862e
Matevž Jekovec <matevz@oasisprotocol.org>
2023-11-17 08:57:58 +0100

    Migrate oasisswag to github.com/oasisprotocol/demo-oasisswag

--- oasisswag/frontend/src/components/ErrorMessage/index.js
+++
@@ -1,9 +0,0 @@
-import classes from "./index.module.css"
-
-export const ErrorMessage = ({children, className}) => {
-  return (
-    <div className={[...(className ? [className] : []), classes.ErrorMessage].join(' ')}>
-      {children}
-    </div>
-  )
-}
--- oasisswag/frontend/src/components/Spinner/index.js
+++
@@ -1,7 +0,0 @@
-import classes from "./index.module.css"
-
-export const Spinner = () => {
-  return (
-    <div className={classes.Spinner} />
-  )
-}
