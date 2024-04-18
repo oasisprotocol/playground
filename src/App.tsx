c3319f9bf3df2e594f40fca280ffd2c7a49d596a
Michal Zielenkiewicz <michal@oasisprotocol.org>
2024-04-18 22:37:03 +0200

    Add new logo in app header

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,14 @@ import { playgroundTheme } from './styles/playgroundTheme';
 import { useTheme } from '@mui/material/styles';
 import { useMediaQuery } from '@mui/material';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from "./assets/Playground Logo White.svg"
+import { styled } from '@mui/material/styles'
+
+const StyledImage = styled('img')({
+  maxWidth: '360px',
+  maxHeight: '60px',
+  marginBottom: '50px'
+})
 
 function App() {
 const theme = useTheme();
@@ -25,9 +33,7 @@ const paddingValue = isMobile ? '42px' : '80px';
       backgroundColor: '#1421F8',
     }}>
       <Container maxWidth="lg" sx={{ paddingTop: paddingValue, paddingBottom: paddingValue }}>
-      <Typography variant="h1" gutterBottom>
-      Oasis Playground
-      </Typography>
+      <StyledImage src={logo} alt="Oasis Playground"/>
       <Grid container spacing={2} justifyContent="space-between" alignItems="flex-start" marginBottom={isMobile ? '36px' : '0'}>
         <Grid item xs={12} md={9}>
           <Typography gutterBottom sx={{ color: 'white', fontSize: '20px', width: '100%', maxWidth: '750px', marginBottom: isMobile ? '16px' : '24px' }}>
