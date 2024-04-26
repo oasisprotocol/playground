b7610d4a8259f6d5e2f52b8074b6d6d8198cb39d
Matevž Jekovec <matevz@oasisprotocol.org>
2024-04-26 12:37:32 +0200

    src: Add support for jpg screenshots

--- src/projectData.ts
+++ src/projectData.ts
@@ -4,11 +4,11 @@ import { load } from "js-yaml";
 
 
 const yamls = import.meta.glob('../projects/*/*.yaml', { as: 'raw', eager: true });
-const allScreenshots = import.meta.glob('../projects/*/screenshots/*.png', {as: 'url', eager: true});
+const allScreenshots = import.meta.glob('../projects/*/screenshots/*.(png|jpg|jpeg)', {as: 'url', eager: true});
 
 export const projects: Project[] = Object.entries(yamls).map(([path, yaml]) => {
 
-const parsedYaml = load(yaml) as Project; 
+const parsedYaml = load(yaml) as Project;
    const screenshotsPath = path.replace(/\/[^/]*?\.yaml$/, '/screenshots' )
    parsedYaml.screenshots = Object.entries(allScreenshots).filter(([screenshotPath]) => screenshotPath.startsWith(screenshotsPath)).map(([_screenshotPath, screenshotUrl]) => screenshotUrl)
    parsedYaml.languages = parsedYaml.languages.map(getCorrectLanguageName)
