f80f95f89f9d801808b1bc35a58f2a61fcbf7e4f
Matevž Jekovec <matevz@oasisprotocol.org>
2024-06-04 12:34:31 +0200

    demo-quiz: Add official demo version

---
+++ demo-quiz/assets/404View.9fae506d.js
@@ -0,0 +1,2 @@
+import{_ as e,o as s,c,a as t}from"./index.8293549d.js";const o={},n={class:"404"},_=t("h1",{class:"text-white text-center text-3xl mb-4"},"404",-1),a=t("h1",{class:"text-white text-center text-2xl"},"Page not found",-1),r=[_,a];function i(x,d){return s(),c("div",n,r)}const l=e(o,[["render",i]]);export{l as default};
+//# sourceMappingURL=404View.9fae506d.js.map
