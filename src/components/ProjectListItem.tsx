80663bd7db2dcbdcc71058fc547d66e3d3521196
Kaja <kaja.osojnik@gmail.com>
2024-10-08 03:14:11 +0200

    Format code with biome

--- src/components/ProjectListItem.tsx
+++ src/components/ProjectListItem.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import { Box, Grid, Paper, Typography, useMediaQuery, useTheme } from '@mui/material';
-import { Project } from '../types';
-import MaintainedByOasisIcon from '../assets/MaintainedByOasisIcon.svg';
-import ProjectItemTags from './ProjectItemTags';
-import ProjectItemLanguages from './ProjectItemLanguages';
+import {
+  Box,
+  Grid,
+  Paper,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material';
+import type React from 'react';
 import ReactMarkdown from 'react-markdown';
 import { Link } from 'react-router-dom';
+import MaintainedByOasisIcon from '../assets/MaintainedByOasisIcon.svg';
+import type { Project } from '../types';
+import ProjectItemLanguages from './ProjectItemLanguages';
+import ProjectItemTags from './ProjectItemTags';
 
 interface ProjectListItemProps {
   project: Project;
@@ -20,7 +27,6 @@ interface ProjectListItemProps {
   langs: string[];
 }
 
-
 const ProjectListItem: React.FC<ProjectListItemProps> = ({
   project,
   getProjectLink,
@@ -31,153 +37,208 @@ const ProjectListItem: React.FC<ProjectListItemProps> = ({
   langs,
   handleTagClick,
   handleLangClick,
-  handleParatimesChange
+  handleParatimesChange,
 }) => {
   const theme = useTheme();
   const isMobileScreen = useMediaQuery(theme.breakpoints.down('sm'));
 
   return (
     <Grid container xs={12} sm={6} md={4}>
-      <Box sx={{display: 'flex', marginBottom: isMobileScreen ? '20px' : '0'}}>
+      <Box
+        sx={{ display: 'flex', marginBottom: isMobileScreen ? '20px' : '0' }}
+      >
         <Paper
-            elevation={3}
-            sx={{
-              margin: isMobileScreen ? '16px 0' : '16px',
-              borderRadius: '16px',
-              border: '2px #0500E1 solid',
-              boxShadow: 'none',
-              height: isMobileScreen ? '100%' : 'auto',
-              overflow: 'hidden'
+          elevation={3}
+          sx={{
+            margin: isMobileScreen ? '16px 0' : '16px',
+            borderRadius: '16px',
+            border: '2px #0500E1 solid',
+            boxShadow: 'none',
+            height: isMobileScreen ? '100%' : 'auto',
+            overflow: 'hidden',
+          }}
+        >
+          <Link
+            to={getProjectLink(project)}
+            style={{
+              textDecoration: 'none',
+              width: '100%',
+              borderTopLeftRadius: '14px',
+              borderTopRightRadius: '14px',
             }}
           >
-            <Link to={getProjectLink(project)} style={{ textDecoration: 'none', width: '100%', borderTopLeftRadius: '14px',
-                  borderTopRightRadius: '14px' }}>
-              <Box 
+            <Box
+              sx={{
+                overflow: 'hidden',
+                height: '33%',
+                borderBottom: '2px #0500E1 solid',
+              }}
+            >
+              <Box
                 sx={{
-                  overflow: 'hidden',
-                  height: '33%',
-                  borderBottom: '2px #0500E1 solid'
-                }}>
-                <Box 
-                  sx={{
-                      transition: 'all .2s ease-in-out',
-                      '&:hover': {
-                        transform: 'scale(1.1)'
-                      }
-                    }}>
-                      <img
-                        src={project.screenshots[0]}
-                        alt={project.name}
-                        style={{
-                          width: '100%',
-                          height: '100%',
-                          objectFit: 'cover',
-                          marginBottom: '8px',
-                        }}
-                      />
-                  </Box>
+                  transition: 'all .2s ease-in-out',
+                  '&:hover': {
+                    transform: 'scale(1.1)',
+                  },
+                }}
+              >
+                <img
+                  src={project.screenshots[0]}
+                  alt={project.name}
+                  style={{
+                    width: '100%',
+                    height: '100%',
+                    objectFit: 'cover',
+                    marginBottom: '8px',
+                  }}
+                />
               </Box>
-            </Link>
-            <Grid container
+            </Box>
+          </Link>
+          <Grid
+            container
             flexDirection={'column'}
             justifyContent={'space-between'}
             wrap={'nowrap'}
             height={'66%'}
-              sx={{
-                padding: '24px',
-                paddingTop: '12px',
-              }}
-            >
-                <Grid item>
-                <Link to={getProjectLink(project)} style={{ textDecoration: 'none' }}>
-                  <Typography 
-                    variant="h2" 
-                    gutterBottom 
-                    sx={{
-                      transition: 'all .1s ease-in-out',
-                      '&:hover': {
-                        fontWeight: '600'
-                      }
-                    }}
-                  >
-                    {project.name}
-                  </Typography>
-                </Link>
-                <Box
+            sx={{
+              padding: '24px',
+              paddingTop: '12px',
+            }}
+          >
+            <Grid item>
+              <Link
+                to={getProjectLink(project)}
+                style={{ textDecoration: 'none' }}
+              >
+                <Typography
+                  variant="h2"
+                  gutterBottom
                   sx={{
-                    color: '#000000',
-                    lineHeight: '130%',
-                    minHeight: '60px',
-                    fontFamily: "'Roboto Flex Variable',sans-serif"
+                    transition: 'all .1s ease-in-out',
+                    '&:hover': {
+                      fontWeight: '600',
+                    },
                   }}
                 >
-                  <ReactMarkdown className='markdown-line-clamp' disallowedElements={['a']}>
-                    {project.description}
-                  </ReactMarkdown>
-                </Box>
-
-                <Box sx={{ minHeight: isMobileScreen ? '50px' : '80px', marginTop: '24px' }}>
-                    <ProjectItemTags tags={tags} selectedTags={selectedTags} isLarge={false} handleTagClick={handleTagClick}/>
-                </Box>
-              </Grid>
-
-              <Grid item container
-                spacing={2}
-                justifyContent={'space-between'}sx={{
-                  marginBottom: isMobileScreen ? '4px' : '0',
-                  marginTop: '0',
-                }}>
-                <Grid item xs={project.maintainedByOasis ? 10 : 12} sx={{ minHeight: isMobileScreen ? '50px' : '80px', paddingRight: '8px', paddingTop: '0' }}>
-                    <Box sx={{display: 'block', width: '100%'}}>
-                      <ProjectItemLanguages langs={langs} selectedLangs={selectedLangs} isLarge={false} isInListItem={true} handleLangClick={handleLangClick}/>
-                    </Box>
-
-                    <Box sx={{display: 'block', width: '100%', paddingTop: '2px'}}>
-                    <Typography sx={{
-                      color: '#000000',
-                      fontSize: '14px'
-                      }}>
-                        {/* ParaTimes: {' '} */}
-                        {project.paratimes.map((paratime: string, index: number) => (
-                          <Typography
-                            component="span"
-                            key={paratime}
-                            onClick={() => handleParatimesChange(paratime)}
-                            sx={{
-                              letterSpacing: '-0.03em', 
-                              color: '#000000',
-                              fontSize: '14px',
-                              cursor: 'pointer',
-                              fontWeight: selectedParatimes.includes(paratime) ? '700' : 'normal',
-                            }}
-                          >
-                            {paratime.charAt(0).toUpperCase() + paratime.slice(1)}
-                            {index < project.paratimes.length - 1 && ', '}
-                          </Typography>
-                        ))}
-                        </Typography>
-                    </Box>
-                </Grid>
-                {project.maintainedByOasis && (
-                <Grid item xs={2}
-                container
-                justifyContent={'right'}
+                  {project.name}
+                </Typography>
+              </Link>
+              <Box
                 sx={{
-                  paddingTop: '0'
+                  color: '#000000',
+                  lineHeight: '130%',
+                  minHeight: '60px',
+                  fontFamily: "'Roboto Flex Variable',sans-serif",
                 }}
+              >
+                <ReactMarkdown
+                  className="markdown-line-clamp"
+                  disallowedElements={['a']}
                 >
-                    <img
-                      width='48px'
-                      src={MaintainedByOasisIcon}
-                      alt="Maintained by Oasis Badge"
-                      style={{ marginTop: isMobileScreen ? '16px' : '0' }}
-                    />
-                </Grid>
-                )}
-              </Grid>
+                  {project.description}
+                </ReactMarkdown>
+              </Box>
 
+              <Box
+                sx={{
+                  minHeight: isMobileScreen ? '50px' : '80px',
+                  marginTop: '24px',
+                }}
+              >
+                <ProjectItemTags
+                  tags={tags}
+                  selectedTags={selectedTags}
+                  isLarge={false}
+                  handleTagClick={handleTagClick}
+                />
+              </Box>
             </Grid>
-          </Paper>
+
+            <Grid
+              item
+              container
+              spacing={2}
+              justifyContent={'space-between'}
+              sx={{
+                marginBottom: isMobileScreen ? '4px' : '0',
+                marginTop: '0',
+              }}
+            >
+              <Grid
+                item
+                xs={project.maintainedByOasis ? 10 : 12}
+                sx={{
+                  minHeight: isMobileScreen ? '50px' : '80px',
+                  paddingRight: '8px',
+                  paddingTop: '0',
+                }}
+              >
+                <Box sx={{ display: 'block', width: '100%' }}>
+                  <ProjectItemLanguages
+                    langs={langs}
+                    selectedLangs={selectedLangs}
+                    isLarge={false}
+                    isInListItem={true}
+                    handleLangClick={handleLangClick}
+                  />
+                </Box>
+
+                <Box
+                  sx={{ display: 'block', width: '100%', paddingTop: '2px' }}
+                >
+                  <Typography
+                    sx={{
+                      color: '#000000',
+                      fontSize: '14px',
+                    }}
+                  >
+                    {/* ParaTimes: {' '} */}
+                    {project.paratimes.map(
+                      (paratime: string, index: number) => (
+                        <Typography
+                          component="span"
+                          key={paratime}
+                          onClick={() => handleParatimesChange(paratime)}
+                          sx={{
+                            letterSpacing: '-0.03em',
+                            color: '#000000',
+                            fontSize: '14px',
+                            cursor: 'pointer',
+                            fontWeight: selectedParatimes.includes(paratime)
+                              ? '700'
+                              : 'normal',
+                          }}
+                        >
+                          {paratime.charAt(0).toUpperCase() + paratime.slice(1)}
+                          {index < project.paratimes.length - 1 && ', '}
+                        </Typography>
+                      ),
+                    )}
+                  </Typography>
+                </Box>
+              </Grid>
+              {project.maintainedByOasis && (
+                <Grid
+                  item
+                  xs={2}
+                  container
+                  justifyContent={'right'}
+                  sx={{
+                    paddingTop: '0',
+                  }}
+                >
+                  <img
+                    width="48px"
+                    src={MaintainedByOasisIcon}
+                    alt="Maintained by Oasis Badge"
+                    style={{ marginTop: isMobileScreen ? '16px' : '0' }}
+                  />
+                </Grid>
+              )}
+            </Grid>
+          </Grid>
+        </Paper>
       </Box>
     </Grid>
   );
--- src/components/TagsSelector.tsx
+++ src/components/TagsSelector.tsx
@@ -6,16 +6,20 @@ interface TagsSelectorProps {
   handleTagClick: (tag: string) => void;
 }
 
-const TagsSelector: React.FC<TagsSelectorProps> = ({ tags, selectedTags, handleTagClick }) => {
+const TagsSelector: React.FC<TagsSelectorProps> = ({
+  tags,
+  selectedTags,
+  handleTagClick,
+}) => {
   return (
     <div>
       {tags.map((tag: string) => (
         <Chip
-            label={tag}
-            key={tag}
-            color={selectedTags.includes(tag) ? 'primary' : 'default'}
-            onClick={() => handleTagClick(tag)}
-            style={{ cursor: 'pointer', marginRight: 4 }}
+          label={tag}
+          key={tag}
+          color={selectedTags.includes(tag) ? 'primary' : 'default'}
+          onClick={() => handleTagClick(tag)}
+          style={{ cursor: 'pointer', marginRight: 4 }}
         />
       ))}
     </div>
