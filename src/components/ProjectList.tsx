5f82304c17972679c06cb5a4dad1f6618e51aeec
Kaja <kaja.osojnik@gmail.com>
2024-10-08 03:45:15 +0200

    Fix biome errors

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -269,7 +269,6 @@ const ProjectList: React.FC = () => {
             (a, b) =>
               new Date(b.created).getTime() - new Date(a.created).getTime(),
           );
-      case SortingOptions.NAME:
       default:
         return projects.slice().sort((a, b) => a.name.localeCompare(b.name));
     }
