88780f647f5927dc5a5f1a7b4fc3438a63647aeb
Matevž Jekovec <matevz@oasisprotocol.org>
2025-03-25 14:52:54 +0100

    biome format

--- src/generate-projects-json.mjs
+++ src/generate-projects-json.mjs
@@ -4,7 +4,9 @@ import { readFileSync, writeFileSync } from 'fs';
 import { getCorrectLanguageName } from './languageUtils.mjs';
 
 const yamls = globSync('./projects/*/*.yaml').sort();
-const allScreenshots = globSync('./projects/*/screenshots/*.{png,jpg,jpeg}').sort();
+const allScreenshots = globSync(
+  './projects/*/screenshots/*.{png,jpg,jpeg}',
+).sort();
 
 /** @type {import('./types').Project[]} */
 const projects = yamls.map((path) => {
@@ -13,7 +15,7 @@ const projects = yamls.map((path) => {
   const screenshotsPath = path.replace(/\/[^/]*?\.yaml$/, '/screenshots');
   parsedYaml.screenshots = allScreenshots
     .filter((screenshotPath) => screenshotPath.startsWith(screenshotsPath))
-    .map(screenshotPath => '/' + screenshotPath)
+    .map((screenshotPath) => '/' + screenshotPath);
   parsedYaml.languages = parsedYaml.languages.map(getCorrectLanguageName);
   const folderName = path.split('/').slice(-2, -1)[0];
   parsedYaml.slug = folderName;
@@ -21,4 +23,8 @@ const projects = yamls.map((path) => {
   return parsedYaml;
 });
 
-writeFileSync('./public/projects.json', JSON.stringify(projects, null, 4), 'utf8');
+writeFileSync(
+  './public/projects.json',
+  JSON.stringify(projects, null, 4),
+  'utf8',
+);
