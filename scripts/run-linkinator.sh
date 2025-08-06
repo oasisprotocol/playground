7ff025355f4aa095a6258856ad982530ffc1fec0
lukaw3d <luka@oasisprotocol.org>
2025-08-06 18:08:41 +0200

    Configure linkinator to ignore some scraping-protected links

--- scripts/run-linkinator.sh
+++ scripts/run-linkinator.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+set -euo pipefail
 
-find projects/ -type f -name "*.yaml" | while read -r file; do
+for file in $(find projects/ -type f -name "*.yaml"); do
   ln -sf "$(realpath --relative-to="$(dirname "$file")" "$file")" "$file.linkinator.md"
 done
 
 # linkinator can parse markdown files, so pretend that yaml files are markdown files.
 # This correctly finds links inside description field (markdown).
-yarn linkinator "projects/**/*.linkinator.md" --concurrency 1
+yarn linkinator "projects/**/*.linkinator.md" --config linkinator.config.json || true
 
 find projects/ -type l -name "*.linkinator.md" -delete
